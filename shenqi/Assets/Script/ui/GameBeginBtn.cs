0e81b2f2d9c1247a2de3beeef36650c183bf3f84
QiaoBin <942764111@qq.com>
2016-11-12 11:52:04 +0800

    更新登录界面

---
+++ shenqi/Assets/Script/ui/GameBeginBtn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameBeginBtn : MonoBehaviour {
+    Transform begin;
+    // Use this for initialization
+    void Start () {
+        begin = transform.Find("bgein");
+        UIEventListener.Get(begin.gameObject).onClick = BeginBtn;
+    }
+    void BeginBtn(GameObject a) {
+        Debug.Log("进入游戏");
+    }
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
